742555ab53f9b1075d533b189967dfe04ba822bf
kangdoohyun <rkdengus1208@gmail.com>
2021-08-03 20:54:41 +0900

    게시물 리스트 타임리프

--- src/main/kotlin/com/kdh/wiken/controller/UsrArticleController.kt
+++ src/main/kotlin/com/kdh/wiken/controller/UsrArticleController.kt
@@ -3,14 +3,19 @@ package com.kdh.wiken.controller
 import com.kdh.wiken.service.ArticleService
 import com.kdh.wiken.vo.Article
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class UsrArticleController (private val articleService : ArticleService) {
-    @RequestMapping("usr/article/getArticles")
-    @ResponseBody
-    fun showMain(): List<Article> {
-        return articleService.getArticles()
+    @RequestMapping("usr/article/list")
+    fun showList(model : Model): String {
+        val articles =  articleService.getArticles()
+
+//        model["articles"] = articles
+        model.set("articles", articles)
+
+        return "usr/article/list"
     }
 }
