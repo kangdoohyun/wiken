7335523789d52f9ea16f97875efb87c1c22439b2
kangdoohyun <rkdengus1208@gmail.com>
2021-08-03 19:51:42 +0900

    타임리프 연결

--- src/main/kotlin/com/kdh/wiken/controller/UsrHomeController.kt
+++ src/main/kotlin/com/kdh/wiken/controller/UsrHomeController.kt
@@ -1,14 +1,15 @@
 package com.kdh.wiken.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class UsrHomeController {
     @RequestMapping("usr/home/main")
-    @ResponseBody
-    fun showMain(): String {
-        return "Hi~ Hello How Are You"
+    fun showMain(model : Model): String {
+        model.addAttribute("name" , "홍길동" )
+        return "usr/home/main"
     }
 }
