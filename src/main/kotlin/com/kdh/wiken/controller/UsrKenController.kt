8af615a14bcadf73955d7ab7da9ce23793962429
kangdoohyun <rkdengus1208@gmail.com>
2021-08-09 13:21:06 +0900

    수정기능 구현

--- src/main/kotlin/com/kdh/wiken/controller/UsrKenController.kt
+++ src/main/kotlin/com/kdh/wiken/controller/UsrKenController.kt
@@ -1,10 +1,11 @@
 package com.kdh.wiken.controller
 
-import com.kdh.wiken.service.ArticleService
 import com.kdh.wiken.service.KenService
 import com.kdh.wiken.vo.Rq
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
@@ -13,12 +14,12 @@ class UsrKenController (private val kenService: KenService) {
     @Autowired
     private lateinit var rq: Rq;
 
-    @RequestMapping("/ken")
+    @RequestMapping("ken")
     fun showWrite(): String {
         return "usr/ken/write"
     }
 
-    @RequestMapping("/ken/doWrite")
+    @RequestMapping("ken/doWrite")
     @ResponseBody
     fun doWrite(title: String, source: String, result: String): String {
         val resultData = kenService.write(1, title, source, result)
@@ -26,5 +27,21 @@ class UsrKenController (private val kenService: KenService) {
         val id = resultData.getData()
 
         return rq.replaceJs("", "../ken/${id}")
+    }
+
+    @RequestMapping("ken/{id}/edit")
+    fun showEdit(@PathVariable("id") id: Int, model: Model): String{
+        val ken = kenService.getKen(id);
+        model.addAttribute("ken", ken)
+
+        return "usr/ken/modify"
+    }
+
+    @RequestMapping("ken/doModify")
+    @ResponseBody
+    fun doModify(id: Int, title: String, source: String, result: String): String {
+        val resultData = kenService.modify(id, title, source, result)
+
+        return rq.replaceJs("", "../ken/${id}/edit")
     }
 }
--- src/main/kotlin/com/kdh/wiken/service/KenService.kt
+++ src/main/kotlin/com/kdh/wiken/service/KenService.kt
@@ -1,5 +1,6 @@
 package com.kdh.wiken.service
 
+import com.jhs.wiken.vo.Ken
 import com.kdh.wiken.repository.ArticleRepository
 import com.kdh.wiken.repository.KenRepository
 import com.kdh.wiken.vo.Article
@@ -13,6 +14,16 @@ class KenService(private val kenRepository: KenRepository) {
         val id = kenRepository.getLastInsertId()
 
         return ResultData.form("S-1", "${id}번 켄이 생성되었습니다.", "id", id)
+    }
+
+    fun getKen(id: Int): Ken? {
+        return kenRepository.getKen(id)
+    }
+
+    fun modify(id: Int, title: String, source: String, result: String): ResultData<Int> {
+        kenRepository.modify(id, title, source, result)
+
+        return ResultData.form("S-1", "${id}번 켄이 수정되었습니다.", "id", id)
     }
 
 }
