571574f2502c3e4beaefe371228715c5816340f7
kangdoohyun <rkdengus1208@gmail.com>
2021-08-09 10:43:03 +0900

    rq를 bean으로 관리

--- src/main/kotlin/com/kdh/wiken/controller/UsrArticleController.kt
+++ src/main/kotlin/com/kdh/wiken/controller/UsrArticleController.kt
@@ -1,6 +1,8 @@
 package com.kdh.wiken.controller
 
 import com.kdh.wiken.service.ArticleService
+import com.kdh.wiken.vo.Rq
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class UsrArticleController (private val articleService : ArticleService) {
+    @Autowired
+    private lateinit var rq: Rq
+
     @RequestMapping("usr/article/list")
     fun showList(model : Model): String {
         val articles =  articleService.getArticles()
--- src/main/kotlin/com/kdh/wiken/interceptor/BeforeActionInterceptor.kt
+++ src/main/kotlin/com/kdh/wiken/interceptor/BeforeActionInterceptor.kt
@@ -1,7 +1,7 @@
 package com.kdh.wiken.interceptor
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.kdh.wiken.vo.Rq
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
@@ -9,13 +9,13 @@ import javax.servlet.http.HttpServletResponse
 
 @Component
 class BeforeActionInterceptor : HandlerInterceptor {
+    @Autowired
+    private lateinit var rq: Rq
+
     override fun preHandle(req: HttpServletRequest, resp: HttpServletResponse, handler: Any): Boolean {
         println("인터셉터 실행")
 
-        val rq = Rq()
-
         rq.setLoginInfo(req.session)
-        req.setAttribute("rq", rq)
 
         return super.preHandle(req, resp, handler)
     }
